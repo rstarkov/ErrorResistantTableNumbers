0430f308c6d04a49735c5531831013d4bcc50d5c
rstarkov <sourcecode@roman.st>
2016-09-17 21:09:38 +0100

    Ignore secondary search criteria (6.3s for 10k trials)

--- Src/Program.cs
+++ Src/Program.cs
@@ -11,9 +11,7 @@ namespace TableNumbers
             var rnd = new Random(12345);
 
             int bestLength = 0;
-            int bestDupes = 0;
-            int bestHighest = int.MaxValue;
-            var bestAssignment = new HashSet<int>();
+            var bestAssignment = new List<int>();
 
             int trials = 0;
             var start = DateTime.UtcNow;
@@ -28,7 +26,7 @@ namespace TableNumbers
                 }
                 var unavailable = new bool[10000];
                 var unavailableCount = 0;
-                var assignment = new HashSet<int>();
+                var assignment = new List<int>();
                 var table = 1;
                 while (unavailableCount < unavailable.Length)
                 {
@@ -57,25 +55,11 @@ namespace TableNumbers
                     table++;
                 }
 
-                var dupes = assignment.Select(a => $"{a:0000}").Sum(a => (a[0] == a[1] ? 1 : 0) + (a[1] == a[2] ? 1 : 0) + (a[2] == a[3] ? 1 : 0));
                 if (bestLength < table)
                 {
                     bestLength = table;
                     bestAssignment = assignment;
-                    bestHighest = assignment.Max();
-                    bestDupes = dupes;
-                    Console.WriteLine($"\r\n{bestLength}, {bestDupes}; {string.Join(",", bestAssignment.OrderBy(a => a))}");
-                }
-                else if (bestLength == table)
-                {
-                    //if (string.Compare(bestHighest, assignment.Max()) > 0)
-                    if (bestDupes < dupes)
-                    {
-                        bestAssignment = assignment;
-                        bestHighest = assignment.Max();
-                        bestDupes = dupes;
-                        Console.WriteLine($"\r\n{bestLength}, {bestDupes}; {string.Join(",", bestAssignment.OrderBy(a => a))}");
-                    }
+                    Console.WriteLine($"\r\n{bestLength}\r\n{string.Join(",", bestAssignment.OrderBy(a => a))}");
                 }
             }
         }
