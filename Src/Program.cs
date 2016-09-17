3d0809a5c50875fe54092f7217d8a8e0806b79f7
rstarkov <sourcecode@roman.st>
2016-09-17 21:38:04 +0100

    Faster: xorshift inlined (from 4.7s to 4.5s for 10k trials)

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Xorshift;
 
 namespace TableNumbers
 {
@@ -9,7 +8,8 @@ namespace TableNumbers
     {
         static void Main(string[] args)
         {
-            var rnd = new RndXorshift(10000);
+            _modulus = 10000;
+            _rejectionLimit = (uint.MaxValue / _modulus) * _modulus;
 
             int bestLength = 0;
             var bestAssignment = new List<int>();
@@ -31,9 +31,24 @@ namespace TableNumbers
                 var table = 1;
                 while (unavailableCount < unavailable.Length)
                 {
-                    var num = (int) rnd.Next();
-                    while (unavailable[num])
-                        num = (int) rnd.Next();
+                    int num;
+                    while (true)
+                    {
+                        if (_bufferPos >= _buffer.Length)
+                        {
+                            _bufferPos = 0;
+                            FillBuffer();
+                        }
+                        uint result = _buffer[_bufferPos];
+                        _bufferPos++;
+                        if (result < _rejectionLimit)
+                        {
+                            num = (int) (result % _modulus);
+                            if (!unavailable[num])
+                                break;
+                        }
+                    }
+
                     for (int digit = 0; digit < 4; digit++)
                     {
                         int mask = 1;
@@ -61,6 +76,36 @@ namespace TableNumbers
                     bestLength = table;
                     bestAssignment = assignment;
                     Console.WriteLine($"\r\n{bestLength}\r\n{string.Join(",", bestAssignment.OrderBy(a => a))}");
+                }
+            }
+        }
+
+        private static uint _modulus;
+        private static uint _rejectionLimit;
+        private static uint[] _buffer = new uint[65536];
+        private static int _bufferPos = int.MaxValue;
+
+        private static uint _x = 123456789;
+        private static uint _y = 362436069;
+        private static uint _z = 521288629;
+        private static uint _w = 88675123;
+
+        private static unsafe void FillBuffer()
+        {
+            fixed (uint* pbytes = _buffer)
+            {
+                uint* pbuf = pbytes;
+                uint* pend = pbytes + _buffer.Length;
+                while (pbuf < pend)
+                {
+                    uint tx = _x ^ (_x << 11);
+                    uint ty = _y ^ (_y << 11);
+                    uint tz = _z ^ (_z << 11);
+                    uint tw = _w ^ (_w << 11);
+                    *(pbuf++) = _x = _w ^ (_w >> 19) ^ (tx ^ (tx >> 8));
+                    *(pbuf++) = _y = _x ^ (_x >> 19) ^ (ty ^ (ty >> 8));
+                    *(pbuf++) = _z = _y ^ (_y >> 19) ^ (tz ^ (tz >> 8));
+                    *(pbuf++) = _w = _z ^ (_z >> 19) ^ (tw ^ (tw >> 8));
                 }
             }
         }
